9527e5d011107b731020d9a5ccd5067cac0dd558
Ana Carolina Petrasso <carolina15petrasso@gmail.com>
2023-09-24 19:57:03 -0300

    made switch btw header sections

--- script/main.js
+++ script/main.js
@@ -1,2 +1,28 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
+
+// Obtén todos los elementos del menú de navegación
+const navItems = $$('.navbarItem');
+
+// Agrega un event listener a cada elemento del menú
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', (event) => {
+    // Evita que se realice la acción predeterminada del enlace
+    event.preventDefault();
+
+    // Obtiene el valor del atributo data-section
+    const sectionId = navItem.getAttribute('data-section');
+
+    // Oculta todas las secciones
+    const sections = $$('.section');
+    sections.forEach((section) => {
+      section.classList.add('hidden');
+    });
+
+    // Muestra la sección correspondiente
+    const targetSection = $(`#${sectionId}`);
+    if (targetSection) {
+      targetSection.classList.remove('hidden');
+    }
+  });
+});
